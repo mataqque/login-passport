459cc02600c01ce84bbcfccec30038d90b199e5a
Flavio <mataqque.100@gmail.com>
2020-01-08 22:58:58 -0500

    mensaje guardado

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+var dbPassword = "mongodb+srv://nombrecuenta:password@cluster0-wxoik.mongodb.net/test?retryWrites=true&w=majority";
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+// const expressLayouts = require('ejs-mate');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const app = express();
+const path = require("path")
+require('./config/passport')(passport);
+const db = require('./config/keys').mongoURI;
+app.use(flash());
+mongoose.connect(db,{ useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+  app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
